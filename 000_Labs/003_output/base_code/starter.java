a8a7cbd91f5dd654a0abe7e7be8937a547da2e61
Potatoman7N <jileedc@yahoo.com>
2026-09-03 19:42:37 +0000

    update

--- 000_Labs/003_output/base_code/starter.java
+++ 000_Labs/003_output/base_code/starter.java
@@ -7,8 +7,11 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
+		// the string "I love dogs." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("Happy Birthday, to you");
+		System.out.println("Happy Birthday, to you");
+		System.out.println("Happy Birthday, dear Nolan");
+		System.out.println("Happy Birthday, to you");
 	}
 }
--- 000_Labs/004_String_class/base_code/starter.java
+++ 000_Labs/004_String_class/base_code/starter.java
@@ -9,6 +9,13 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String Guy = ("Dear Nolan!");
+		String Smile = ("Happy Birthday, ");
+		String It = ("to you!");
+		System.out.println(Smile + It); 
+		System.out.println(Smile + It);
+		System.out.println(Smile + Guy);
+		System.out.println(Smile + It);
+
 	}
 }
