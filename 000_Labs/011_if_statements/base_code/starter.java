0c3a5380619b5870bf9023d553f0ca2a1e671886
Potatoman7N <jileedc@yahoo.com>
2026-09-22 19:06:36 +0000

    Update

--- 000_Labs/011_if_statements/base_code/starter.java
+++ 000_Labs/011_if_statements/base_code/starter.java
@@ -9,6 +9,16 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		int cheese = 50;
+		int bum = 150;
+		boolean answer = cheese == bum;
+		System.out.println("The first variable is " + cheese);
+		System.out.println("The second variable is " + bum);
+		if(answer == true){
+			System.out.println("The variables are the same");
+		}
+		if(answer == false){
+			System.out.println("The variables are different");
+		}
 	}
 }
--- 000_Labs/012_if_statements_more/base_code/starter.java
+++ 000_Labs/012_if_statements_more/base_code/starter.java
@@ -9,6 +9,17 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please input your first number: ");
+		int num1 = sc.nextInt();
+		System.out.print("Please input your second number: ");
+		int num2 = sc.nextInt();
+		boolean answer = num1 == num2;
+		if(answer == true){
+			System.out.println("The numbers are the same!");
+		}
+		if(answer == false){
+			System.out.println("The numbers are different!");
+		}
 	}
 }
