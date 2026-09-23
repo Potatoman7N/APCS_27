51f3b691ef940e95ab57a914d057df340dc6da6d
Potatoman7N <jileedc@yahoo.com>
2026-09-23 19:10:46 +0000

    Update

--- 000_Labs/014_else/base_code/starter.java
+++ 000_Labs/014_else/base_code/starter.java
@@ -10,6 +10,13 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		int taco = (int)(Math.random()*15);
+		if(taco>2){
+			System.out.print("Greater");
+		}
+		else{
+			System.out.print("Smaller");
+		}
+		
 	}
 }
